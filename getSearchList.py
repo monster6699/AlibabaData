3332a5c9d944d6fdcf02d80f6bda7498e057fc29
monster6699 <1241340047@qq.com>
2021-02-26 18:08:58 +0800

    'update'

--- getSearchList.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-
-def search(keyword):
-    requests.get()
--- start.py
+++ start.py
@@ -1,40 +1,73 @@
+import sys
 import time
 
 import requests
+import xlwt
 from lxml import etree
 from selenium import webdriver
 
-def start():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'
-    }
-    # data = requests.get(url="https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText=jacket", headers=headers)
-    url = "https://www.alibaba.com"
-    driver = webdriver.Chrome(executable_path="./webdriver/chromedriver.exe")
-    driver.get(url)
-    search = driver.find_element_by_class_name("ui-searchbar-keyword")
-    search.send_keys("apple")
-    button = driver.find_element_by_xpath('//*[@id="J_SC_header"]/header/div[2]/div[3]/div/div/form/input[4]')
-    driver.execute_script("arguments[0].click();", button)
 
-    # 将滚动条移动到页面的底部
-    js = "var q=document.documentElement.scrollTop=100000"
-    driver.execute_script(js)
-    time.sleep(3)
+class ContentFocus:
+    def __init__(self):
+        self.search_data = sys.argv[1]
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'
+        }
+        self.url = "https://www.alibaba.com"
 
+        self.urls_list = []
 
-    html = etree.HTML(driver.page_source)
+    def request_url_to_save_excel(self, url_list):
+        # 创建一个excel
+        excel = xlwt.Workbook()
+        # 添加工作区
+        sheet = excel.add_sheet("数据")
 
+        # 标题信息
+        head = ["标题", "关键词"]
+        for index, value in enumerate(head):
+            sheet.write(0, index, value)
 
-    # parse_html = html.xpath('//*[@id="root"]//div[@class="offer-list-wrapper"]//div[@class="list-no-v2-left"]/a/@href')
-    parse_html = html.xpath('//*[@id="root"]//div[@class="offer-list-wrapper"]//div[@data-content="abox-ProductNormalList"]//div/a[@class="organic-gallery-offer__img-section"]/@href')
-    print(len(parse_html))
-    for detail_url in parse_html:
-        data = requests.get("https:" + detail_url, headers=headers)
-        detail = etree.HTML(data.text)
-        detail_data = detail.xpath('/html/head/meta[@name="keywords"]/@content')
-        print(detail_data)
+        for index, detail_url in enumerate(url_list, 1):
+            data = requests.get("https:" + detail_url, headers=self.headers)
+            detail = etree.HTML(data.text)
+            detail_data = detail.xpath('/html/head/meta[@name="keywords"]/@content')
+            value_list = detail_data[0].split("- Buy")
+            for i, value in enumerate(value_list):
+                sheet.write(index, i, value)
+
+        # 保存excel
+        excel.save("./write.xls")
+
+    def start(self):
+        driver = webdriver.Chrome(executable_path="./webdriver/chromedriver.exe")
+        driver.get(self.url)
+        search = driver.find_element_by_class_name("ui-searchbar-keyword")
+        print(self.search_data)
+        search.send_keys(self.search_data)
+        button = driver.find_element_by_xpath('//*[@id="J_SC_header"]/header/div[2]/div[3]/div/div/form/input[4]')
+        driver.execute_script("arguments[0].click();", button)
+        while True:
+            time.sleep(5)
+            pagination = driver.find_element_by_xpath(
+                '//*[@id="root"]/div//div//a[@class="seb-pagination__pages-link pages-next"]')
+            driver.execute_script("arguments[0].scrollIntoView();", pagination)
+            html = etree.HTML(driver.page_source)
+            parse_html = html.xpath(
+                '//*[@id="root"]//div[@class="offer-list-wrapper"]//div[@data-content="abox-ProductNormalList"]//div/a[@class="organic-gallery-offer__img-section"]/@href')
+            for detail_url in parse_html:
+                if len(self.urls_list) < 100:
+                    self.urls_list.append(detail_url)
+
+            if len(self.urls_list) < 100:
+                driver.execute_script("arguments[0].click();", pagination)
+                continue
+            else:
+                break
+        driver.close()
+        self.request_url_to_save_excel(self.urls_list)
 
 
 if __name__ == '__main__':
-    start()
+    content = ContentFocus()
+    content.start()
